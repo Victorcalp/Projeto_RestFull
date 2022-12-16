57343cc33d49d49bf27185a14588a2dbeeba1418
Victor <victorcalp@outlook.com.br>
2022-12-16 16:22:18 -0300

    ajustando controller

--- Projeto_RestFull/Controllers/PersonController.cs
+++ Projeto_RestFull/Controllers/PersonController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Projeto_RestFull.Business;
-using Projeto_RestFull.Business.Implementations;
 using Projeto_RestFull.Model;
-using Projeto_RestFull.Model.Context;
 
 namespace Projeto_RestFull.Controllers
 {
@@ -11,25 +10,51 @@ namespace Projeto_RestFull.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+        private readonly ILogger<PersonController> _logger;
         private IPerson _person;
 
-        public PersonController(IPerson person)
+        public PersonController(ILogger<PersonController> logger, IPerson person)
         {
+            _logger = logger;
             _person = person;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult FiendAll()
         {
             var result = _person.FiendAll();
             return Ok(result);
         }
 
-        [HttpGet("id")]
-        public IActionResult Get(int id)
+        [HttpGet("{id}")]
+        public IActionResult FiendById(int id)
         {
             var result = _person.FiendByID(id);
+            if (result == null) return NotFound();
             return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            var result = _person.Create(person);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            var result = _person.Update(person);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _person.Delete(id);
+            return NoContent();
         }
     }
 }
