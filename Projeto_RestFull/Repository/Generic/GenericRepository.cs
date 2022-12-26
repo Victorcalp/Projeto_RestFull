642b5cf651e0a13fadea295f8c2d1ac790aa7af4
Victor <victorcalp@outlook.com.br>
2022-12-26 16:09:08 -0300

    adicionado GenericRepository

---
+++ Projeto_RestFull/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_RestFull.Model;
+using Projeto_RestFull.Model.Base;
+using Projeto_RestFull.Model.Context;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Projeto_RestFull.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly MySQLContext _context;
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+        public T Update(T item)
+        {
+            var result = dataset.SingleOrDefault(x => x.Id.Equals(item.Id));
+
+            if (!Exists(item.Id)) new Person();
+            try
+            {
+                dataset.Update(result);
+                _context.SaveChanges();
+                return result;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Delete(long id)
+        {
+            var result = dataset.SingleOrDefault(x => x.Id.Equals(id));
+
+            if (!Exists(id)) new Person();
+            try
+            {
+                dataset.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<T> FiendAll()
+        {
+            return dataset.ToList();
+        }
+
+        public T FiendByID(long id)
+        {
+            return dataset.SingleOrDefault(x => x.Id.Equals(id));
+        }
+        public bool Exists(long id)
+        {
+            return dataset.Any(x => x.Id.Equals(id));
+        }
+    }
+}
---
+++ Projeto_RestFull/Repository/Generic/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Projeto_RestFull.Model.Base;
+using System.Collections.Generic;
+
+namespace Projeto_RestFull.Repository.Generic
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T Update(T item);
+        void Delete(long id);
+        T FiendByID(long id);
+        List<T> FiendAll();
+        bool Exists(long id);
+    }
+}
